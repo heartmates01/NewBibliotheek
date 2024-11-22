461df24f19a540a431c691db731728d5d3bde980
heartmates01 <kiryuchameke@gmail.com>
2024-11-22 11:41:09 +0100

    overview text fix + borrow/return prep

--- src/main/java/nl/heartmates01/magazine/MonthlyMag.java
+++ src/main/java/nl/heartmates01/magazine/MonthlyMag.java
@@ -22,31 +22,33 @@ public class MonthlyMag extends Magazine {
   private final String publisher;
   private final String copyEditor;
   private final int pages;
+  private boolean borrowed;
   private final String issn;
   private final int issueNumber;
   private final LocalDate publicationDate;
 
   protected MonthlyMag(long id, String title, String publisher, String copyEditor, int pages,
+      boolean borrowed,
       String issn, int issueNumber, LocalDate publicationDate) {
-    super(id, title, publisher, copyEditor, pages, issn, issueNumber, publicationDate);
+    super(id, title, publisher, copyEditor, pages, borrowed, issn, issueNumber, publicationDate);
 
     this.id = id;
     this.title = title;
     this.publisher = publisher;
     this.copyEditor = copyEditor;
     this.pages = pages;
+    this.borrowed = borrowed;
     this.issn = issn;
     this.issueNumber = issueNumber;
     this.publicationDate = publicationDate;
   }
 
   public int getMonthNumber() {
-    return issueNumber;
+    return publicationDate.getMonthValue();
   }
 
   // Voor MonthlyMagazine: "Issn: {issn} - Title: {title} - Month: {monthNumber}"
   public String getOverviewText() {
-    return "Issn: " + issn + "\nTitle: " + title + "\nIssue: " + issueNumber + "\nMonth: "
-        + getMonthNumber();
+    return "\nIssn: " + issn + "\nTitle: " + title + "\nIssue: " + issueNumber + "\nMonth: "
+        + getMonthNumber() + "\nBorrowed: " + borrowed;
   }
-}
