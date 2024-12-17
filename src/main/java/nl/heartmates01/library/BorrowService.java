635ce67e3551b28e85c54af184d9ab16fe9741bc
heartmates01 <kiryuchameke@gmail.com>
2024-12-17 12:04:17 +0100

    17-12-24

--- src/main/java/nl/heartmates01/library/BorrowService.java
+++ src/main/java/nl/heartmates01/library/BorrowService.java
@@ -4,12 +4,13 @@ package nl.heartmates01.library;
 // Deze class is verantwoordelijk voor het terugbrengen en uitlenen van alle items.
 // In plaats van dat de BookController en andere controllers dit doen, gaat het nu via de BorrowService.
 
-// methods;
-// daily borrow/return
-// weekly borrow/return
-// monthly borrow/return
-// + move borrow/return from book
-
 public class BorrowService {
 
+  public static void borrowItem(Item item) {
+    item.borrow();
+  }
+
+  public static void returnItem(Item item) {
+    item.returnn();
+  }
 }
