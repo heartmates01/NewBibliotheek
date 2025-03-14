7f64d1742acdc7d9f94056d0e170c8278b631f77
heartmates01 <kiryuchameke@gmail.com>
2025-03-14 15:14:44 +0100

    updated book

--- src/main/java/nl/heartmates01/book/Book.java
+++ src/main/java/nl/heartmates01/book/Book.java
@@ -1,121 +1,107 @@
 package nl.heartmates01.book;
 
-// De class Book heeft de volgende members (velden)
-// LETOP: Andere classes mogen niet direct bij deze members kunnen:
-
-// id (long)
-// title (String)
-// author (String)
-// pages (int)
-// isbn (String)
-// borrowed (boolean)
-
-// De class Book heeft de volgende methods:
-
-// long getId()
-
-// boolean hasBeenBorrowed()
-// Deze method geeft de waarde van borrowed terug.
-
-// String getTitleWithAuthor()
-// Deze method geeft de titel en de auteur van het boek terug als een String.
-
-// String toString()
-// Deze method geeft alle informatie van het boek terug als een String
-// (tip: gebruik een String template """).'
-
-// void borrowBook()
-// Deze method zet de borrowed waarde op true.
-
-// void returnBook()
-// Deze method zet de borrowed waarde op false.
-
-// + all under (String) toString()
-
-// (long) id
-// |_ (long) getId()
-
-// (String) title
-// |_ (String) getTitleWithAuthor()
-
-// (String) author
-// |_ (String) getTitleWithAuthor()
-
-// (int) pages
-
-// (String) isbn
-
-// (boolean) borrowed
-// |_ (boolean) hasBeenBorrowed()
-// |_ (void) borrowBook()
-// |_ (void) returnBook()
-
 import java.time.LocalDate;
 import nl.heartmates01.library.Item;
 
 // overerft Item
 public class Book extends Item {
 
-  protected static int id;
-  private static int count;
-  private String title;
-  private String author;
-  private int pages;
+  protected int id;
+  private final String title;
+  private final Author author;
+  private final int pages;
   private boolean borrowed;
-  private long isbn;
-  protected static int borrowTime;
-  private LocalDate publicationDate;
+  private final long isbn;
+  protected int borrowTime;
+  private final LocalDate publicationDate;
 
-  Book(int id, String title, String author, int pages, boolean borrowed, long isbn, int borrowTime,
+  // for bookform
+  Book(String title, Author author, int pages, boolean borrowed, long isbn,
       LocalDate publicationDate) {
-    super(id, title, pages, borrowed, borrowTime, publicationDate);
-
-    Book.id = ++count;
+    super(title, pages, borrowed, publicationDate);
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.borrowed = borrowed;
     this.isbn = isbn;
-    Book.borrowTime = 0;
     this.publicationDate = publicationDate;
   }
 
-  // Voor Book: "Isbn: {isbn} - Title: {title} - Author: {author}"
-  public String getOverviewText() {
-    return "Isbn: " + isbn + "\nTitle :" + title + "\nAuthor: " + author;
+  Book(int id, String title, Author author, int pages, boolean borrowed, long isbn,
+      LocalDate publicationDate) {
+    super(title, pages, borrowed, publicationDate);
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.borrowed = borrowed;
+    this.isbn = isbn;
+    this.publicationDate = publicationDate;
   }
 
-  long getId() {
+  public String getOverviewText() {
+    return "\nIsbn: " + isbn + "\nTitle : " + title + "\nAuthor: " + author.getName() + "\n";
+  }
+  
+  int getId() {
     return id;
   }
 
-  // Deze method geeft de waarde van borrowed terug.
+  public void setId(int id) {
+    this.id = id;
+  }
+
+
+  String getTitle() {
+    return title;
+  }
+
+  Author getAuthor() {
+    return author;
+  }
+
+  int getPages() {
+    return pages;
+  }
+
+  boolean getBorrowed() {
+    return borrowed;
+  }
+
+  long getIsbn() {
+    return isbn;
+  }
+
+  LocalDate getPublicationDate() {
+    return publicationDate;
+  }
+
   public boolean hasBeenBorrowed() {
     return borrowed;
   }
 
-  // Deze method geeft de titel en de auteur van het boek terug als een String.
   public String getTitleWithAuthor() {
-    return "\n" + title + "\n" + author + "\n";
+    return "\n" + title + "\n" + author.getName() + "\n";
   }
-
-  // Deze method geeft alle informatie van het boek terug als een String
-// (tip: gebruik een String template """).'
+  
   public String toString() {
-    return "\n" + "ID = " + id + "\n" + "Title = " + title + "\n" + "Author = " + author + "\n"
+    return "\n" + "ID = " + id + "\n" + "Title = " + title + "\n" + "Author = " + author.getName()
+        + "\n"
         + "Pages = " + pages + "\n" + "ISBN = " + isbn + "\n" + "Borrowed = " + borrowed + "\n"
-        + "\n" + "Publication date = " + publicationDate;
+        + "Publication Date = " + publicationDate + "\n";
   }
+  
+  // ill add borrow & return soon promise.
 
-  public void borrow() {
-    borrowed = true;
-    borrowTime = 14;
-    System.out.println("Book has been borrowed.");
-  }
-
-  public void returnn() {
-    borrowed = false;
-    borrowTime = 0;
-    System.out.println("Book has been returned.");
-  }
+//  public void borrow() {
+//    borrowed = true;
+//    borrowTime = 14;
+//    System.out.println("Book has been borrowed.");
+//  }
+//
+//  public void returnn() {
+//    borrowed = false;
+//    borrowTime = 0;
+//    System.out.println("Book has been returned.");
+//  }
 }
