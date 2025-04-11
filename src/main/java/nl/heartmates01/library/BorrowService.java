a546334a885be888aca0048786c404a9fb24d559
heartmates01 <kiryuchameke@gmail.com>
2025-04-11 14:05:39 +0200

    Update BorrowService.java

--- src/main/java/nl/heartmates01/library/BorrowService.java
+++ src/main/java/nl/heartmates01/library/BorrowService.java
@@ -1,14 +1,12 @@
 package nl.heartmates01.library;
 
-// Maak een class BorrowService.
-// Deze class is verantwoordelijk voor het terugbrengen en uitlenen van alle items.
-// In plaats van dat de BookController en andere controllers dit doen, gaat het nu via de BorrowService.
-
-import static nl.heartmates01.Main.userInput;
-
-import java.util.regex.Pattern;
-
+//acts as repository
 public class BorrowService {
+
+  //sonarqube recommended this
+  private BorrowService() {
+    throw new IllegalStateException("Utility class BorrowService; java:S1118");
+  }
 
   public static void borrowItem(Item item) {
     item.borrow();
@@ -16,29 +14,5 @@ public class BorrowService {
 
   public static void returnItem(Item item) {
     item.returnn();
-  }
-
-  public static void borrowOrReturn(Item item) {
-    System.out.println("""
-        \s
-        Library Management System
-        \s
-        1. Borrow
-        2. Return
-        3. Exit to magazine menu""");
-
-    int borrowOrReturn = Integer.parseInt(
-        userInput("Choose an option from the list.", Pattern.compile("[0-3]"),
-            "Choose a valid option."));
-    if (borrowOrReturn == 3) {
-      System.out.println("Exiting to magazine menu.");
-      return;
-
-    } else if (borrowOrReturn == 1) {
-      BorrowService.borrowItem(item);
-
-    } else if (borrowOrReturn == 2) {
-      BorrowService.returnItem(item);
-    }
   }
 }
