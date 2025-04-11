39295b75a932ff7fc4de2a95d1190e503be60dbf
heartmates01 <kiryuchameke@gmail.com>
2025-04-11 14:09:12 +0200

    Update BookController.java

--- src/main/java/nl/heartmates01/book/BookController.java
+++ src/main/java/nl/heartmates01/book/BookController.java
@@ -2,6 +2,7 @@ package nl.heartmates01.book;
 
 import static nl.heartmates01.main.Main.authorRepository;
 import static nl.heartmates01.main.Main.bookRepository;
+import static nl.heartmates01.main.Main.publisherRepository;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,12 +11,18 @@ import nl.heartmates01.main.Main;
 
 public class BookController {
 
+  //sonarqube recommended this
+  private BookController() {
+    throw new IllegalStateException("Utility class BookController; java:S1118");
+  }
+
   static final List<Map<String, Runnable>> bookOptions = List.of(
       Map.of("Add a Book", BookController::addBook),
       Map.of("Remove a Book", BookController::removeBook),
       Map.of("List singular Book", BookController::getFromId),
+      Map.of("Borrow or Return Book", BookController::borrowOrReturn),
       Map.of("List all Books", BookController::listAllBooks),
-      Map.of("Search Books by Author", BookController::searchByAuthor),
+      Map.of("Search Books by Keyword", BookController::searchByKeyword),
       Map.of("Exit", () -> System.exit(0))
   );
 
@@ -44,7 +51,8 @@ public class BookController {
         (Integer) userInput.get("pages"),
         (Boolean) userInput.get("borrowed"),
         (Long) userInput.get("isbn"),
-        (LocalDate) userInput.get("publicationDate")
+        (LocalDate) userInput.get("publicationDate"),
+        publisherRepository.get((Integer) userInput.get("pubId")).get()
     ));
     System.out.println(userInput.get("title") + " has been added.");
   }
@@ -57,18 +65,22 @@ public class BookController {
 
   public static void getFromId() {
     int id = Integer.parseInt(Main.getUserInput("Enter this book's ID: "));
-    bookRepository.get(id).ifPresentOrElse(book -> System.out.println(book.getOverviewText()),
-        () -> System.out.println("Book not found."));
+    bookRepository.get(id).ifPresent(book -> System.out.println(book.toString()));
+  }
+
+  //'Book book' doesn't work 'cause of Optional
+  public static void borrowOrReturn() {
+    int id = Integer.parseInt(Main.getUserInput("Enter this book's ID: "));
+//    Book book = bookRepository.get(id);
+//    borrowController.borrowOrReturn(book);
   }
 
   public static void listAllBooks() {
     bookRepository.getAll().forEach(book -> System.out.println(book.toString()));
   }
 
-  public static void searchByAuthor() {
-    int author = Integer.parseInt(Main.getUserInput("Enter the author's ID: "));
-    bookRepository.search(author)
-        .ifPresentOrElse(books -> books.forEach(book -> System.out.println(book.getOverviewText())),
-            () -> System.out.println("Book not found."));
+  public static void searchByKeyword() {
+    String keyword = Main.getUserInput("Enter the Keyword: ");
+    bookRepository.search(keyword).forEach(book -> System.out.println(book.toString()));
   }
 }
