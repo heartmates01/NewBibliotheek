00e7132957f6b9879a88a13ffee14fd95afc2ae9
heartmates01 <kiryuchameke@gmail.com>
2025-04-11 14:12:17 +0200

    Create Boardgame.java

---
+++ src/main/java/nl/heartmates01/boardgame/Boardgame.java
@@ -0,0 +1,108 @@
+package nl.heartmates01.boardgame;
+
+import java.time.LocalDate;
+import nl.heartmates01.library.Item;
+import nl.heartmates01.magazine.Publisher;
+
+public class Boardgame extends Item {
+
+  protected int id;
+  private final String title;
+  private final String designer;
+  private final String ean;
+  private final Publisher publisher;
+  private final LocalDate publicationDate;
+  private final int minPlayers;
+  private final int maxPlayers;
+  private boolean borrowed;
+
+  //without id for boardgameform
+  Boardgame(String title, String designer, String ean, Publisher publisher,
+      LocalDate publicationDate, int minPlayers,
+      int maxPlayers, boolean borrowed) {
+    super(title, publicationDate, borrowed);
+    this.title = title;
+    this.designer = designer;
+    this.ean = ean;
+    this.publisher = publisher;
+    this.publicationDate = publicationDate;
+    this.minPlayers = minPlayers;
+    this.maxPlayers = maxPlayers;
+  }
+
+  //with id for add
+  Boardgame(int id, String title, String designer, String ean, Publisher publisher,
+      LocalDate publicationDate, int minPlayers,
+      int maxPlayers, boolean borrowed) {
+    super(title, publicationDate, borrowed);
+    this.id = id;
+    this.title = title;
+    this.designer = designer;
+    this.ean = ean;
+    this.publisher = publisher;
+    this.publicationDate = publicationDate;
+    this.minPlayers = minPlayers;
+    this.maxPlayers = maxPlayers;
+  }
+
+  public String getOverviewText() {
+    return "\nID: " + id + "\nTitle: " + title + "\nDesigner: " + designer + "\nPublisher: "
+        + publisher.getName() + "\nPublication Date: " + publicationDate + "\nEAN: " + ean
+        + "\nMin. Players: " + minPlayers + "\nMax. Players: " + maxPlayers;
+  }
+
+  public String toString() {
+    return "\nTitle: " + title + "\nDesigner: " + designer + "\nPublisher: "
+        + publisher.getName() + "\nPublication Date: " + publicationDate + "\nEAN: " + ean + "\n";
+  }
+
+  int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  String getTitle() {
+    return title;
+  }
+
+  String getDesigner() {
+    return designer;
+  }
+
+  String getEan() {
+    return ean;
+  }
+
+  Publisher getPublisher() {
+    return publisher;
+  }
+
+  LocalDate getPublicationDate() {
+    return publicationDate;
+  }
+
+  int getMinPlayers() {
+    return minPlayers;
+  }
+
+  int getMaxPlayers() {
+    return maxPlayers;
+  }
+
+  boolean getBorrowed() {
+    return borrowed;
+  }
+
+  public void borrow() {
+    borrowed = true;
+    System.out.println("Boardgame has been borrowed.");
+  }
+
+  public void returnn() {
+    borrowed = false;
+    System.out.println("Boardgame has been returned.");
+  }
+}
